a4ceae334f3067abffbc05dbbb178454c4bc3304
JustRec <mmmollaoglumehmet@gmail.com>
2021-12-18 13:00:03 +0300

    fix uppercase problem

--- Program.cs
+++ Program.cs
@@ -32,13 +32,14 @@ namespace regex
         static string[] FoundScore(string[] words, string pattern, int word_count){
             string[] elligable_words = new string[word_count];
             
+            
             for (int i = 0; i < word_count; i++)
             {
                 bool flag = true;
                 if(words[i].Length == pattern.Length){
                     for (int j = 0; j < pattern.Length; j++)
                     {
-                        if(pattern[j] != '-' && pattern[j] != words[i][j]){
+                        if(pattern[j] != '-' && pattern.ToLower()[j] != words[i].ToLower()[j]){
                             flag = false;
                         }
                     }
@@ -59,11 +60,7 @@ namespace regex
             int word_count;            
 
             input_text = "Miss Polly had a poor dolly, who was sick. She called for the talled doctor Solly to come quick. He knocked on the DOOR like a actor in the healthcare sector.";
-            pattern = "-olly";
-
-            
-            
-            input_text = input_text.ToLower();
+            pattern = "-olly";           
             
             string[] words = input_text.Split();
 
@@ -90,7 +87,6 @@ namespace regex
                     System.Console.WriteLine(final_words[i]);
                 }
             }
-            
         }   
     }
 }
