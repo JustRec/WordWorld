f87bd1cc6e1390429f50b73cec338b390a4d6428
JustRec <mmmollaoglumehmet@gmail.com>
2021-12-19 20:18:31 +0300

    add comments and user input

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace regex
+namespace word_search
 {
     class Program
     {
@@ -8,13 +8,13 @@ namespace regex
 
             char[] punctuations = new char[]{'.', ',', '!', '?', '(', ')', '"', '-', ':', ';', '[', ']'};
 
-            for (int i = 0; i < words.Length; i++)
-            {
+            //Remove any punctuation, use an extra variable to escape outofindex errors
+            for (int i = 0; i < words.Length; i++){
                 bool exception_escape = false;
-                for (int j = 0; j < words[i].Length; j++)
-                {
-                    for (int k = 0; k < 12; k++)
-                    {
+
+                for (int j = 0; j < words[i].Length; j++){
+
+                    for (int k = 0; k < 12; k++){
                         if(words[i][j] == punctuations[k]){
                             words[i] = words[i].Remove(j,1);
                             exception_escape = true;
@@ -30,43 +30,44 @@ namespace regex
             return words;
         }
         static string[] FoundScore(string[] words, string pattern, int word_count){
-            string[] elligable_words = new string[word_count];
+            string[] fitting_words = new string[word_count];
             
-            
+            //Chec
             for (int i = 0; i < word_count; i++)
             {
+                //Assign current word to another array if the current character is either matches with the pattern or is score(-) char
                 bool flag = true;
                 if(words[i].Length == pattern.Length){
-                    for (int j = 0; j < pattern.Length; j++)
-                    {
+                    for (int j = 0; j < pattern.Length; j++){
                         if(pattern[j] != '-' && pattern.ToLower()[j] != words[i].ToLower()[j]){
                             flag = false;
                         }
 
-                        for (int k = 0; k < i; k++)
-                        {
-                            if (elligable_words[k] == words[i]){
+                        //Check if there is duplicate words
+                        for (int k = 0; k < i; k++){
+                            if (fitting_words[k] == words[i]){
                                 flag = false;
                             }
                         }
                     }
                     if(flag){
                         
-                        elligable_words[i] = words[i];
+                        fitting_words[i] = words[i];
                     }
                 }
             }
 
-            return elligable_words;
+            return fitting_words;
         }
 
-        static int[] StarIndexer(string pattern, int word_count){
+        static int[] StarIndexer(string pattern, int word_count){ //This function saves star chars positions in an array
             int[] starindex_temp = new int[word_count];
             int counter = 0;
 
-            for (int i = 0; i < pattern.Length; i++)
-            {
+            //Assign star position to an array
+            for (int i = 0; i < pattern.Length; i++){
                 if(pattern[i] == '*'){
+                    //Assign a negative value if the 0 index possess a star character
                     if(i == 0){
                         starindex_temp[0] = -1;
                     }
@@ -78,18 +79,19 @@ namespace regex
                 }
             }
 
+            //Count how many star positions are registired
             counter = 0;
-            for (int i = 0; i < word_count; i++)
-            {
+            for (int i = 0; i < word_count; i++){
                     if(starindex_temp[i] != 0){
                         counter++;
                     } 
             }
 
+            //Intruduce a new variable with correct size
             int[] starindex = new int[counter];
 
-            for (int i = 0; i < word_count; i++)
-            {
+            //Fill new array with the values saved before
+            for (int i = 0; i < word_count; i++){
                 if(starindex_temp[i] != 0){
                     starindex[i] = starindex_temp[i];
                 }
@@ -98,6 +100,7 @@ namespace regex
                 }
             }
 
+            //Fıx a special event which happens when the first character of the pattern is a star
             if(starindex[0] == -1){
                 starindex[0] = 0;
             }
@@ -106,90 +109,98 @@ namespace regex
 
         }
         static string[] FoundStar(string[] words, string pattern, int word_count){
-            string[] elligable_words = new string[word_count];
+            string[] fitting_words = new string[word_count];
             int[] starindex = new int[word_count];
             string[] substring = new string[pattern.Length];
 
             starindex = StarIndexer(pattern, word_count);
             int counter = 0;
-
+            //If statement that only executes if the pattern has one star at the beginning of the string
             if(pattern[0] == '*' && pattern.Length != 1 && starindex.Length == 1){
                 string special_substring = pattern.Substring(1).ToLower();
                 
-                for (int i = 0; i < word_count; i++)
-                {
+                for (int i = 0; i < word_count; i++){
                     if(words[i].ToLower().IndexOf(special_substring) != - 1){
                         if(words[i].IndexOf(special_substring) == words[i].Length - special_substring.Length){
-                            elligable_words[i] = words[i];
+                            fitting_words[i] = words[i];
                         }  
                     }                     
                 }
             }
+
+            //If statement that only executes if the pattern has one star at the end of the string
             else if(pattern[pattern.Length - 1] == '*' && pattern.Length != 1 && starindex.Length == 1){
                 string special_subtring = pattern.Substring(0,pattern.Length - 1).ToLower();
 
-                for (int i = 0; i < word_count; i++)
-                {
+                for (int i = 0; i < word_count; i++){
                     if(words[i].ToLower().IndexOf(special_subtring) == 0){
-                        elligable_words[i] = words[i];
+                        fitting_words[i] = words[i];
                     }
                 }
             }
             
+            //If statement that only executes if the pattern is a single star string
             else if(pattern[0] == '*' && pattern.Length == 1){
-                elligable_words = words;
+                fitting_words = words;
             }
 
+            //This part covers all the other cases
             else{
+
+                //Saves the substring that start at the start of the string and ends at the first star
                 substring[counter] = pattern.Substring(0, starindex[0]);
                 counter++;
 
+                //Saves the substrings that is stuck between stars
                 for (int i = 0; i < starindex.Length - 1; i++){
                     substring[counter] = pattern.Substring(starindex[i]+1, (starindex[i+1] - starindex[i])-1);
                     counter ++;
                 }            
 
+                //Saves the substring that start at the the last star and ends at the end of the string
                 substring[counter] = pattern.Substring((starindex[starindex.Length - 1])+1);
                 counter++;
 
-                for (int i = 0; i < word_count; i++)
-                {
+                //Assign words to an array if the word contains required substrings.
+                for (int i = 0; i < word_count; i++){
                     bool flag = true;
-                    for (int j = 0; j < counter; j++)
-                    {
+
+                    for (int j = 0; j < counter; j++){
                         if(!words[i].ToLower().Contains(substring[j].ToLower()) && substring[j] != null){
                             flag = false;
                             break;
                         }
-                        for (int k = 0; k < substring.Length - 1; k++)
-                        {
+
+                        //Check the order of the substring and do not assign the word if the order is wrong
+                        for (int k = 0; k < substring.Length - 1; k++){
+
                             if(substring[k] != null && substring[k+1] != null){
                                 if(words[i].IndexOf(substring[k]) > words[i].IndexOf(substring[k + 1])){
                                     flag = false;
                                     break;
                                 }
                             }
-
                         }
                     }
-                    if(flag){
-                        elligable_words[i] = words[i];
-                    }
 
+                    if(flag){
+                        fitting_words[i] = words[i];
+                    }
                 }
             }
-            for (int i = 0; i < elligable_words.Length; i++)
-            {
-                for (int j = 0; j < elligable_words.Length; j++)
-                {
-                    if(elligable_words[i] == elligable_words [j] && i != j){
-                        elligable_words[i] = null;
+
+            //Remove duplicate words in the array
+            for (int i = 0; i < fitting_words.Length; i++){
+                
+                for (int j = 0; j < fitting_words.Length; j++){
+
+                    if(fitting_words[i] == fitting_words [j] && i != j){
+                        fitting_words[i] = null;
                         break;
                     }
                 }
-            }
-            
-            return elligable_words;
+            }            
+            return fitting_words;
         }
         static void Main(string[] args){
             Console.Clear();
@@ -198,13 +209,20 @@ namespace regex
             string pattern;
             int word_count;            
 
-            input_text = "Miss Polly had a poor dolly, who was sick. She called for the talled doctor Solly to come quick. He knocked on the DOOR like a actor in the healthcare sector.";
-            pattern = "*";           
+            //Take user inputs
+            Console.Write("Please enter the input text: ");
+            input_text = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Please enter the search pattern: ");
+            pattern = Console.ReadLine();
+            Console.WriteLine();
             
+            //Split input text into a word array
             string[] words = input_text.Split();
 
             word_count = words.Length;
 
+            //Check if the pattern consist star or score
             bool IsPatternContainsScore = false;
             bool IsPatternContainsStar = false;
             for(int i = 0; i < pattern.Length; i++ ){
@@ -216,25 +234,24 @@ namespace regex
                 }
             }
 
+            //Call PunctuationRemover and remove any punctuation characters
             words = PunctuationRemover(words);
 
+            //Call the correct function according the IsPAtternContains variables
             if(IsPatternContainsScore){
                 words = FoundScore(words, pattern, word_count);
             }
             else if(IsPatternContainsStar){
                 words = FoundStar(words, pattern, word_count);
             }
+
+            //Output the words while excluding null values
             for (int i = 0; i < word_count; i++)
             {
                 if(words[i] != null){
                     System.Console.WriteLine(words[i]);
                 }
             }
-
         }   
     }
 }
-
-//To-Do
-//substring confuse
-//single star
